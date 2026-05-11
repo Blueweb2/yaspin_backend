17192fde60201ba749b55dff577b00a46b66a12d
Sabithabasima <sabithabasimavk@gmail.com>
2026-05-11 15:30:26 +0530

    publicid can store  images with &

--- src/utils/sendImageToCloudinary.ts
+++ src/utils/sendImageToCloudinary.ts
@@ -7,13 +7,24 @@ const sendImageToCloudinary = async (
   buffer: Buffer,
   folder: string
 ): Promise<UploadApiResponse> => {
+  // SANITIZE IMAGE NAME
+
+  const sanitizedImageName =
+    imageName
+      .toLowerCase()
+      .replace(/&/g, "and")
+      .replace(/[^a-z0-9]/g, "-")
+      .replace(/-+/g, "-")
+      .replace(/^-|-$/g, "");
+
   return new Promise(
     (resolve, reject) => {
       const stream =
         cloudinary.uploader.upload_stream(
           {
             folder,
-            public_id: imageName,
+            public_id:
+              sanitizedImageName,
           },
           (error, result) => {
             if (error) {
